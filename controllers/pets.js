b85459aa1f7fc1dabf37a62c93216f73129738fa
stevenmondragon23 <smondragonflores@byupathway.edu>
2026-05-27 20:45:43 -0500

    try and catch

--- controllers/pets.js
+++ controllers/pets.js
@@ -4,74 +4,233 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async(req,res) => {
     //swagger.tags=['pets']
-    const result = await mongodb.getDatabase().db().collection('pets').find();
-    result.toArray().then((pets) => {
-        res.setHeader('Content-Type' , 'application/json');
+    
+    try{
+    const result = await mongodb
+        .getDatabase()
+        .db()
+        .collection('pets')
+        .find();
+
+
+
+        const pets = await result.toArray();
+
+        res.setHeader('Content-Type', 'application/json');
         res.status(200).json(pets);
-    });
+    
+
+
+    } catch(error){
+        res.status(500).json({
+            message: error.message
+        });
+    }
 };
+
+
+//_____________________________________________________________
+
+
+
+
 
 
 
 const getSingle = async(req,res) => {
     //swagger.tags=['pets']
+
+    try{
+
+    
     const petId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('pets').find({_id: petId});
-    result.toArray().then((pets) => {
-        res.setHeader('Content-Type' , 'application/json');
+    const result = await mongodb
+        .getDatabase()
+        .db()
+        .collection('pets')
+        .find({_id: petId});
+
+
+        const pets = await result.toArray();
+
+        if (!pets[0]) {
+            return res.status(404).json({
+                message: 'Pet not found'
+            });
+        }
+
+        res.setHeader('Content-Type', 'application/json');
         res.status(200).json(pets[0]);
-    });
+
+
+
+    } catch{
+        res.status(500).json({
+            message: error.message
+        });
+    }
 };
 
 
-const createPet = async(req, res) =>{
+//------------------------------
+
+
+const createPet = async (req, res) => {
     //swagger.tags=['pets']
-    const pet ={
-        petName: req.body.petName,
-        petSpecie: req.body.petSpecie,
-        petAge: req.body.petAge,
-        petOwnerName: req.body.petOwnerName,
-        petOwnerPhone: req.body.petOwnerPhone,
-        weight: req.body.weight
-    };
-    const response = await mongodb.getDatabase().db().collection('pets').insertOne(pet);
-    if (response.acknowledged >0){
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating pet');
+
+    try {
+
+        const pet = {
+            petName: req.body.petName,
+            petSpecie: req.body.petSpecie,
+            petAge: req.body.petAge,
+            petOwnerName: req.body.petOwnerName,
+            petOwnerPhone: req.body.petOwnerPhone,
+            weight: req.body.weight
+        };
+
+        // VALIDATION
+        if (
+            !pet.petName ||
+            !pet.petSpecie ||
+            !pet.petAge ||
+            !pet.petOwnerName ||
+            !pet.petOwnerPhone ||
+            !pet.weight
+        ) {
+            return res.status(400).json({
+                message: 'Please provide all required fields'
+            });
+        }
+
+        const response = await mongodb
+            .getDatabase()
+            .db()
+            .collection('pets')
+            .insertOne(pet);
+
+        if (response.acknowledged) {
+
+            res.status(201).json({
+                message: 'Pet created successfully'
+            });
+
+        } else {
+
+            res.status(500).json({
+                message: 'Some error occurred while creating pet'
+            });
+
+        }
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
     }
 };
 
-const updatePet = async(req, res) =>{
-    //swagger.tags=['pet']
-    const petId = new ObjectId(req.params.id);
-    const pet ={
-        petName: req.body.petName,
-        petSpecie: req.body.petSpecie,
-        petAge: req.body.petAge,
-        petOwnerName: req.body.petOwnerName,
-        petOwnerPhone: req.body.petOwnerPhone,
-        weight: req.body.weight
-    };
-    const response = await mongodb.getDatabase().db().collection('pets').replaceOne({ _id: petId}, pet);
-    if (response.modifiedCount >0){
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the user');
+
+//---------------------------------------------
+
+
+const updatePet = async (req, res) => {
+    //swagger.tags=['pets']
+
+    try {
+
+        const petId = new ObjectId(req.params.id);
+
+        const pet = {
+            petName: req.body.petName,
+            petSpecie: req.body.petSpecie,
+            petAge: req.body.petAge,
+            petOwnerName: req.body.petOwnerName,
+            petOwnerPhone: req.body.petOwnerPhone,
+            weight: req.body.weight
+        };
+
+        // VALIDATION
+        if (
+            !pet.petName ||
+            !pet.petSpecie ||
+            !pet.petAge ||
+            !pet.petOwnerName ||
+            !pet.petOwnerPhone ||
+            !pet.weight
+        ) {
+            return res.status(400).json({
+                message: 'Please provide all required fields'
+            });
+        }
+
+        const response = await mongodb
+            .getDatabase()
+            .db()
+            .collection('pets')
+            .replaceOne({ _id: petId }, pet);
+
+        if (response.modifiedCount > 0) {
+
+            res.status(204).send();
+
+        } else {
+
+            res.status(500).json({
+                message: 'Some error occurred while updating pet'
+            });
+
+        }
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
     }
 };
 
 
-const deletePet = async(req, res) => {
-    //swagger.tags=['pet']
-    const petId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('pets').deleteOne({ _id: petId});
-    if (response.deletedCount >0){
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the user');
+//-----------------------------------------
+
+const deletePet = async (req, res) => {
+    //swagger.tags=['pets']
+
+    try {
+
+        const petId = new ObjectId(req.params.id);
+
+        const response = await mongodb
+            .getDatabase()
+            .db()
+            .collection('pets')
+            .deleteOne({ _id: petId });
+
+        if (response.deletedCount > 0) {
+
+            res.status(204).send();
+
+        } else {
+
+            res.status(500).json({
+                message: 'Some error occurred while deleting pet'
+            });
+
+        }
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
     }
-}
+};
+
+//----------------------------------------------------------------------------
 
 
 module.exports = {
